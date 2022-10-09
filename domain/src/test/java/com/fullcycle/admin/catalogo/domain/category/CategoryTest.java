e05b27a7ebef075d186b3e95d22ea860d76b1da6
Nilton Morais <niltonmorais.code@gmail.com>
2022-10-09 19:30:46 -0300

    criando entidade Category

---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
@@ -0,0 +1,26 @@
+package com.fullcycle.admin.catalogo.domain.category;
+
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+
+    @Test
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "Categoria de filmes";
+        final boolean expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertNotNull(expectedName, actualCategory.getName());
+        Assertions.assertNotNull(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+}
