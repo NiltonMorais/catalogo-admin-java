fb2423e35cf88911f94901f5f43fa23377c5fdbb
Nilton Morais <niltonmorais.code@gmail.com>
2022-10-09 13:45:01 -0300

    removendo pasta .idea

--- src/main/java/com/fullcycle/admin/catalogo/Main.java
+++ src/main/java/com/fullcycle/admin/catalogo/Main.java
@@ -1,4 +1,6 @@
-package com.fullcycle.admin.catalogo;public class Main {
+package com.fullcycle.admin.catalogo;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
